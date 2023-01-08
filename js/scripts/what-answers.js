88eff2f60b304818886da35ba1dd261866b3a6f0
mikscust <mikaela.c@protonmail.ch>
2023-01-09 07:56:14 +0800

    added javascript comment explanation

--- js/scripts/what-answers.js
+++ js/scripts/what-answers.js
@@ -1,6 +1,12 @@
+// generates a random text answer when the 'what' button is clicked.
+
+// whatButton is a reference to the 'what' button element in the DOM.
 const whatButton = document.querySelector(".what-generate-text-answer-button");
+
+// whatOutput is a reference to the element where the generated text answer will be displayed.
 const whatOutput = document.querySelector(".what-text-answer-output");
 
+// whatAnswerTexts is an array of different types of text answers that can be generated.
 const whatAnswerTexts = [
     "love",
     "confession",
@@ -28,7 +34,12 @@ const whatAnswerTexts = [
     "funny"
 ];
 
+// event listener for the 'click' event on the 'whatButton' element. when the button is clicked, a random text answer is generated.
 whatButton.addEventListener('click', function() {
+
+    // generates a random index for the 'whatAnswerTexts' array.
     const randomWhatAnswerIndex = Math.floor(Math.random() * whatAnswerTexts.length);
+    
+    // sets the text content of the 'whatOutput' element to the text answer at the random index generated above.
     whatOutput.textContent = whatAnswerTexts[randomWhatAnswerIndex];
 });
--- js/scripts/who-answers.js
+++ js/scripts/who-answers.js
@@ -1,6 +1,12 @@
+// generates a random text answer when the 'who' button is clicked.
+
+// whoButton is a reference to the 'who' button element in the DOM.
 const whoButton = document.querySelector(".who-generate-text-answer-button");
+
+// whoOutput is a reference to the element where the generated text answer will be displayed.
 const whoOutput = document.querySelector(".who-text-answer-output");
 
+// whoAnswerTexts is an array of different types of text answers that can be generated.
 const whoAnswerTexts = [
     "family member",
     "partner",
@@ -19,7 +25,12 @@ const whoAnswerTexts = [
     "anyone"
 ];
 
+// event listener for the 'click' event on the 'whoButton' element. when the button is clicked, a random text answer is generated.
 whoButton.addEventListener('click', function() {
+
+    // generates a random index for the 'whoAnswerTexts' array.
     const randomWhoAnswerIndex = Math.floor(Math.random() * whoAnswerTexts.length);
+
+    // sets the text content of the 'whoOutput' element to the text answer at the random index generated above.
     whoOutput.textContent = whoAnswerTexts[randomWhoAnswerIndex];
 });
